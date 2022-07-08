8777ed59f39b5248b18feb3cc34f168f5083c415
EdgarLefevre <EdgarLefevre@users.noreply.github.com>
2022-07-08 07:18:32 +0000

    :construction_worker: Automated Commit

--- src/scripts_for_paper/prepare_dataset_multi_threads.py
+++ src/scripts_for_paper/prepare_dataset_multi_threads.py
@@ -12,67 +12,74 @@ import skimage
 import skimage.io as io
 
 ##################################################################################
-########################## DATA PREPARATION ######################################      
-################################################################################## 
+########################## DATA PREPARATION ######################################
+##################################################################################
+
 
 def list_files(path):
     return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
+
 def list_files_path(path):
     return [path + f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
+
 def list_paths(path):
-    '''
+    """
     List all the paths in a directory
-    '''
+    """
     paths = []
     for root, dirs, files in os.walk(path):
         paths.extend(os.path.join(root, file) for file in files)
     return paths
 
+
 def get_number(path):
     filename = os.path.basename(path)
     return int(filename.split("_")[0])
 
+
 def enhance_contrast(image):
-    image = skimage.exposure.equalize_adapthist(image/255, clip_limit=0.03)
+    image = skimage.exposure.equalize_adapthist(image / 255, clip_limit=0.03)
     return (image * 255).astype(np.uint8)
+
 
 def process_img(im_path, img_indexes, labels_path, save_path):
     img = io.imread(im_path, plugin="tifffile").astype(np.uint8)
     for i, index in enumerate(img_indexes):
-        if f'{str(index)}.tif' in labels_path:
+        if f"{str(index)}.tif" in labels_path:
             print(index)
             if index > 11136:  # 11136 = separator of two batches
-                io.imsave(save_path + str(index)+".tif", img[i])
+                io.imsave(save_path + str(index) + ".tif", img[i])
             else:
-                io.imsave(save_path + str(index)+".tif", enhance_contrast(img[i]))
-                
+                io.imsave(save_path + str(index) + ".tif", enhance_contrast(img[i]))
+
+
 def img_to_slices():
     PATHS = ["Raw_data/"]
-    
+
     tiff_path = []
     for path in PATHS:
         tiff_path += list_files_path(path)
-    
+
     LAB_PATH = "Lungs/Labels/"
     l = list_files(LAB_PATH)
-    
+
     LAB_PATH_metas = "Metastases/Labels/"
     l_metas = list_files(LAB_PATH_metas)
-    
+
     for img_path in tiff_path:
         file_nb = get_number(img_path)
         i = 128 * file_nb
-        img_indexes = range(i, i+128)
+        img_indexes = range(i, i + 128)
         process_img(img_path, img_indexes, l, "Lungs/Images/")
         process_img(img_path, img_indexes, l_metas, "Metastases/Images/")
-        
-        
+
+
 ##################################################################################
-########################## DATA AUGMENTATION #####################################      
-##################################################################################        
-        
+########################## DATA AUGMENTATION #####################################
+##################################################################################
+
 
 def rotate_img(path, img_name):
     img = cv2.imread(path + img_name, cv2.IMREAD_UNCHANGED)
@@ -120,7 +127,9 @@ def elastic_transform(image, alpha, sigma, alpha_affine, random_state=None):
         -alpha_affine, alpha_affine, size=pts1.shape
     ).astype(np.float32)
     M = cv2.getAffineTransform(pts1, pts2)
-    image = cv2.warpAffine(image, M, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101)
+    image = cv2.warpAffine(
+        image, M, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101
+    )
 
     dx = filters.gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
     dy = filters.gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
@@ -154,8 +163,9 @@ def elastic_transform_wrapped(img, mask, path_img, path_mask):
     im_mask_t = im_merge_t[..., 1]
     io.imsave(path_img + "t_" + img, im_t)
     io.imsave(path_mask + "t_" + mask, im_mask_t)
-    
-def process_DA(path_img, path_label):  
+
+
+def process_DA(path_img, path_label):
     list_img = list_files(path_img)
     list_mask = list_files(path_label)
 
@@ -172,38 +182,54 @@ def process_DA(path_img, path_label):
         print("transform " + img)
         mask = list_mask[i]
         elastic_transform_wrapped(img, mask, path_img, path_label)
-        
+
+
 def main_DA():
     img_folders = ["Lungs/Images/", "Metastases/Images/"]
     mask_folders = ["Lungs/Labels/", "Metastases/Labels/"]
     for i, img_path in enumerate(img_folders):
         mask_path = mask_folders[i]
         process_DA(img_path, mask_path)
-        
+
+
 ##################################################################################
 ########################## Multi Processing ######################################
-##################################################################################     
+##################################################################################
+
 
 def multi_process_fun(file_list, dirpath, function, num_workers):
     list_size = len(file_list)
-    worker_amount = int(list_size/num_workers)
+    worker_amount = int(list_size / num_workers)
 
     processes = []
     for worker_num in range(num_workers):
         if worker_num == range(num_workers)[-1]:
-            process = multiprocessing.Process(target=function, args=(dirpath, file_list[worker_amount*worker_num:]))   
+            process = multiprocessing.Process(
+                target=function, args=(dirpath, file_list[worker_amount * worker_num :])
+            )
         else:
-            process = multiprocessing.Process(target=function, args=(dirpath, file_list[worker_amount*worker_num : worker_amount*worker_num + worker_amount]))
+            process = multiprocessing.Process(
+                target=function,
+                args=(
+                    dirpath,
+                    file_list[
+                        worker_amount * worker_num : worker_amount * worker_num
+                        + worker_amount
+                    ],
+                ),
+            )
         processes.append(process)
         process.start()
 
     for process in processes:
         process.join()
 
+
 def wrapped_rotate(path, file_list):
     for file in file_list:
         print(file)
         rotate_img(path, file)
+
 
 def wrapped_elastic_transformation(path, file_list):
     """Wrapped elastic transform for multiprocessing.
@@ -214,9 +240,10 @@ def wrapped_elastic_transformation(path, file_list):
     """
     path_img, path_mask = path
     for (img, mask) in file_list:
-         elastic_transform_wrapped(img, mask, path_img, path_mask)
-         
-def process_DA_multi(path_img, path_label, n_workers):  
+        elastic_transform_wrapped(img, mask, path_img, path_mask)
+
+
+def process_DA_multi(path_img, path_label, n_workers):
     list_img = list_files(path_img)
     list_mask = list_files(path_label)
 
@@ -226,15 +253,22 @@ def process_DA_multi(path_img, path_label, n_workers):
     list_img = list_files(path_img)
     list_mask = list_files(path_label)
 
-    multi_process_fun(list(zip(list_img, list_mask)), (path_img, path_label), wrapped_elastic_transformation, n_workers)
-    
+    multi_process_fun(
+        list(zip(list_img, list_mask)),
+        (path_img, path_label),
+        wrapped_elastic_transformation,
+        n_workers,
+    )
+
+
 def main_DA_multi(n_worker):
-    img_folders = ["3classes/Images/"]  #"Lungs/Images/", "Metastases/Images/"]
-    mask_folders = ["3classes/Labels/"]  #"Lungs/Labels/", "Metastases/Labels/"]
+    img_folders = ["3classes/Images/"]  # "Lungs/Images/", "Metastases/Images/"]
+    mask_folders = ["3classes/Labels/"]  # "Lungs/Labels/", "Metastases/Labels/"]
     for i, img_path in enumerate(img_folders):
         mask_path = mask_folders[i]
         process_DA_multi(img_path, mask_path, n_worker)
-        
+
+
 def get_args():
     """
     Argument parser.
@@ -244,27 +278,35 @@ def get_args():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--n_workers", type=int, default=4, help="number of thread used to run the script."
+        "--n_workers",
+        type=int,
+        default=4,
+        help="number of thread used to run the script.",
     )
     return parser.parse_args()
 
+
 ##################################################################################
 ########################## 3 classes masks #######################################
-##################################################################################  
+##################################################################################
+
 
 def get_healthy_mice(csv_path):
-    '''
+    """
     Get the healthy mice from the csv file
-    '''
+    """
     df = pd.read_csv(csv_path)
-    return df.loc[df['Saine/Metas'] == 's']['Id'].values
-    
+    return df.loc[df["Saine/Metas"] == "s"]["Id"].values
+
+
 def create_new_mask(mask_lungs, mask_metas):
     assert np.shape(mask_lungs) == np.shape(mask_metas)
     return np.where((mask_metas == 1), 2, mask_lungs)
 
+
 def get_number_3classes(name):
-    return np.trunc(int(name.split(".")[0])/128)
+    return np.trunc(int(name.split(".")[0]) / 128)
+
 
 def multi_classes_train():
     PATH_LUNGS = "Lungs/Labels/"
@@ -278,9 +320,9 @@ def multi_classes_train():
     healthy_mice_list = get_healthy_mice("mouse_ID.csv")
     paths_lungs = list_paths(PATH_LUNGS)
     for lung_path in paths_lungs:
-        name = lung_path.split('/')[-1]
+        name = lung_path.split("/")[-1]
         lung_mask = io.imread(lung_path) / 255
-        print(100*'-')
+        print(100 * "-")
         print(name)
         try:
             meta_mask = io.imread(PATH_METAS + name) / 255
@@ -289,35 +331,36 @@ def multi_classes_train():
             io.imsave(new_path + name, new_mask)
         except Exception as e:
             print(e)
-            print(f'{name} meta not found.')
+            print(f"{name} meta not found.")
             nb = get_number_3classes(name)
             if nb in healthy_mice_list:
-                print(f'{name} is healthy')
+                print(f"{name} is healthy")
                 os.system(f"cp {PATH_IMGS}{name} {new_path_imgs}")
-                io.imsave(new_path+name, lung_mask)
+                io.imsave(new_path + name, lung_mask)
+
 
 def multi_classes_test():
     BASE_PATH = "Test/"
     name_list = ["m2Pc_c1_10Corr_1", "8_m2PL_day25", "28_2Pc_day50", "56_2PLc_day106"]
-    for name in name_list: 
+    for name in name_list:
         print(name)
         os.system(f"mkdir -p {BASE_PATH+name}/3classes/")
         paths_metas = list_paths(f"{BASE_PATH+name}/metas/")
         assert len(paths_metas) != 0, f"{BASE_PATH+name}/metas/"
         for meta_path in paths_metas:
-            f_name = meta_path.split('/')[-1]
-            try :
+            f_name = meta_path.split("/")[-1]
+            try:
                 meta_mask = io.imread(meta_path) / 255
                 assert np.max(meta_mask) <= 1
                 lung_mask = io.imread(f"{BASE_PATH}{name}/lungs/{f_name}") / 255
                 assert np.max(lung_mask) <= 1
                 new_mask = create_new_mask(lung_mask, meta_mask)
-                io.imsave(f"{BASE_PATH+name}/3classes/"+f_name, new_mask)
+                io.imsave(f"{BASE_PATH+name}/3classes/" + f_name, new_mask)
             except Exception as e:
                 print(f_name)
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     start = time.time()
     args = get_args()
     img_to_slices()
--- src/scripts_for_paper/var_inter_obs.py
+++ src/scripts_for_paper/var_inter_obs.py
@@ -30,7 +30,9 @@ def stats_pixelbased(y_true, y_pred):
     """
     y_pred = y_pred.reshape(128, 128)
     if y_pred.shape != y_true.shape:
-        raise ValueError(f"Shape of inputs need to match. Shape of prediction is: {y_pred.shape}.  Shape of y_true is: {y_true.shape}")
+        raise ValueError(
+            f"Shape of inputs need to match. Shape of prediction is: {y_pred.shape}.  Shape of y_true is: {y_true.shape}"
+        )
 
     pred = y_pred
     truth = y_true
@@ -45,29 +47,34 @@ def stats_pixelbased(y_true, y_pred):
     # Fmeasure = (2 * precision * recall) / (precision + recall)
     return -2 if union.sum() == 0 else intersection.sum() / union.sum()
 
+
 def ratio(mask1, mask2):
     mask1 = np.array(mask1)
     mask2 = np.array(mask2)
     den = np.maximum(mask1.sum(), mask2.sum())
     num = np.minimum(mask1.sum(), mask2.sum())
-    return 1.-(num/den) if den != num else 0.
+    return 1.0 - (num / den) if den != num else 0.0
+
 
 def diff(mask1, mask2):
     mask1 = np.array(mask1)
     mask2 = np.array(mask2)
     _max = np.maximum(mask1.sum(), mask2.sum())
     _min = np.minimum(mask1.sum(), mask2.sum())
-    return _max-_min
+    return _max - _min
+
 
 def sorted_alphanumeric(data):
     convert = lambda text: int(text) if text.isdigit() else text.lower()  # noqa
     alphanum_key = lambda key: [convert(c) for c in re.split("([0-9]+)", key)]  # noqa
     return sorted(data, key=alphanum_key)
 
+
 def list_files_path(path):
     return sorted_alphanumeric(
         [path + f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     )
+
 
 def main(path1, path2):
     file_list1 = list_files_path(path1)
@@ -75,10 +82,10 @@ def main(path1, path2):
     res_ratio = []
     res_iou = []
     res_diff = []
-    for i, file1 in enumerate(file_list1): 
+    for i, file1 in enumerate(file_list1):
         file2 = file_list2[i]
-        im_em = io.imread(file1, plugin="tifffile")/255
-        im_ed = io.imread(file2, plugin="tifffile")/255
+        im_em = io.imread(file1, plugin="tifffile") / 255
+        im_ed = io.imread(file2, plugin="tifffile") / 255
 
         # print("SUM : {} em ; {} ed \nratio = {}".format(im_em.sum(), im_ed.sum(), ratio(im_em, im_ed) ))
         # print("SUM : {} em ; {} ed \ndiff = {}".format(im_em.sum(), im_ed.sum(), diff(im_em, im_ed) ))
@@ -92,17 +99,16 @@ def main(path1, path2):
     print(f"mean iou {np.array(res_iou).mean()}")
     print(f"mean diff {np.array(res_diff).mean()}")
 
+
 if __name__ == "__main__":
     BASE_PATH_TEST = "/home/edgar/Documents/Datasets/deepmeta/deepmeta_dataset/Test/"
     BASE_PATH_ANNOT = "/home/edgar/Documents/Datasets/deepmeta/Test_annotation_2/"
-    
+
     mice = ["m2Pc_c1_10Corr_1", "56_2PLc_day106", "28_2Pc_day50", "8_m2PL_day25"]
     for mouse in mice:
         print(f"{mouse} lungs")
-        main(BASE_PATH_TEST+mouse+"/lungs/", BASE_PATH_ANNOT+mouse+"/Lungs/")
+        main(BASE_PATH_TEST + mouse + "/lungs/", BASE_PATH_ANNOT + mouse + "/Lungs/")
         print("\n")
         print(f"{mouse} Metas")
-        main(BASE_PATH_TEST+mouse+"/metas/", BASE_PATH_ANNOT+mouse+"/Metas/")
-        print(100*'-')
-    
-    
+        main(BASE_PATH_TEST + mouse + "/metas/", BASE_PATH_ANNOT + mouse + "/Metas/")
+        print(100 * "-")
